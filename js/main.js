79cef00d5e754ef85caf3adfd44d89289bb74427
FabrizioFavia <personal.favia@gmail.com>
2023-04-19 15:40:23 +0200

    feat(html/css/js): created proj / clas / eventLis.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let nextBtn = document.getElementById("buttonRight");
+let backBtn = document.getElementById("buttonLeft");
+let slideContainer = document.getElementById("imgBox");
+slideContainer.createElement
+nextBtn.addEventListener("click", nextSlide);
+nextBtn.addEventListener("click", previousSlide);
+
+
+function nextSlide(){
+    alert("ciao");
+}
