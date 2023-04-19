528e662d39b1dc50e8c15b2d4636eb97c6799624
FabrizioFavia <personal.favia@gmail.com>
2023-04-19 19:04:25 +0200

    feat(js): created second for loop

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 let nextBtn = document.getElementById("buttonRight");
 let backBtn = document.getElementById("buttonLeft");
-var slideContainer = document.getElementById("imgBox");
-let slideElement = document.getElementsByClassName("slide")
-let slideArray = ["01.webp", "02.webp", "03.webp", "04.webp", "05.webp"];
 
+var slideContainer = document.getElementById("imgBox");
+let slideElements = document.getElementsByClassName("slide")
+let slideArray = ["01.webp", "02.webp", "03.webp", "04.webp", "05.webp"];
+let currentSlide = 0;
 
 
 for (let i = 0; i < slideArray.length; i++) {
@@ -22,9 +23,36 @@ for (let i = 0; i < slideArray.length; i++) {
     }
 }
 
+nextBtn.addEventListener("click", function () {
+
+    for (let i = 0; i < slideElements.length; i++) {
+        const slide = slideElements[i];
+
+        if (i == currentSlide + 1) {
+            slide.classList.remove("hidden");
+        } else {
+            slide.classList.add("hidden");
+        }
+    }
+    currentSlide++;
+});
+
+backBtn.addEventListener("click", function () {
+
+    for (let i = 0; i < slideElements.length; i++) {
+        const slide = slideElements[i];
+
+        if (i == currentSlide - 1) {
+            slide.classList.remove("hidden");
+        } else {
+            slide.classList.add("hidden");
+        }
+    }
+    currentSlide--;
+});
 
 
 
 
-nextBtn.addEventListener("click", nextSlide);
-nextBtn.addEventListener("click", previousSlide);
+
+
