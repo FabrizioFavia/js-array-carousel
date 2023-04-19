7ea128f4e75920e818ab8b43a58b5131f16272ca
FabrizioFavia <personal.favia@gmail.com>
2023-04-19 17:55:41 +0200

    feat(js): created array

--- js/main.js
+++ js/main.js
@@ -1,11 +1,30 @@
 let nextBtn = document.getElementById("buttonRight");
 let backBtn = document.getElementById("buttonLeft");
-let slideContainer = document.getElementById("imgBox");
-slideContainer.createElement
+var slideContainer = document.getElementById("imgBox");
+let slideElement = document.getElementsByClassName("slide")
+let slideArray = ["01.webp", "02.webp", "03.webp", "04.webp", "05.webp"];
+
+
+
+for (let i = 0; i < slideArray.length; i++) {
+
+
+    if (i == 0) {
+        let x = document.createElement("img");
+        x.src = `../img/${slideArray[i]}`;
+        x.className = "slide";
+        slideContainer.append(x);
+    } else {
+        let x = document.createElement("img");
+        x.src = `../img/${slideArray[i]}`;
+        x.className = "slide hidden";
+        slideContainer.append(x);
+    }
+}
+
+
+
+
+
 nextBtn.addEventListener("click", nextSlide);
 nextBtn.addEventListener("click", previousSlide);
-
-
-function nextSlide(){
-    alert("ciao");
-}
